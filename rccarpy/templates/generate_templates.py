6dd71dcb6c24452ab585dafc42539f3669d8fb73
Musyoka Bumgil <mbumgil@gmail.com>
2020-06-11 00:14:44 +0900

    Update generate_templates.py
    generates images for use as templates for template matching

--- rccarpy/templates/generate_templates.py
+++ rccarpy/templates/generate_templates.py
@@ -1,10 +1,11 @@
 import random
-import os
 import cv2
 import numpy as np
+from time import sleep
 
 
-def random_noise(image):
+
+def rand_noise(image):
 	rows = image.shape[0]
 	cols = image.shape[1]
 	mean = 0
@@ -25,26 +26,43 @@ def random_noise(image):
 	noisy_image = noisy_image.astype(np.uint8)
 	return noisy_image
 
-def horizontal_flip(image):
-    flipped = cv2.flip(image, 0)
-    return flipped
-
 def rotate(image):
-	
-	deg = random.randint(0, 120)
+
+	deg = random.randint(-90, 90)
 	rows = image.shape[0]
 	cols = image.shape[1]
-	print(rows, cols)
+	#print(rows, cols)
 	M = cv2.getRotationMatrix2D((cols / 2, rows / 2), deg, 2)
 	img_rot = cv2.warpAffine(image, M, (cols, rows))
+
 	return img_rot
 
+def reSize(image):
+	h,w = image.shape[0], image.shape[1]
+	print(h,w)
+	rand_val = random.randint(-100, 100)
+	resized = cv2.resize(image, ((h+rand_val), (w+rand_val)))
+	return resized
 
-image = cv2.imread('sample.jpg')
-#image = cv2.resize(image, (1024, 768 ))
-modified = rotate(image)
-flip = horizontal_flip(image)
-noise_img = random_noise(image)
-cv2.imwrite('saved.jpg', noise_img)
-cv2.imshow("img", noise_img)
+def flipped(image):
+	flippedImage = np.flip(image, 1)
+	return flippedImage
+
+image = cv2.imread('template.jpg')
+
+for i in range(1,21): #180 rotations 180 pics
+	modifiedImg = rotate(image)
+	resizedImg = reSize(image)
+	noisyImg = rand_noise(image)
+	cv2.imwrite(f'generated_template_Images/saved_{i}.jpg', noisyImg)
+	cv2.imwrite(f'generated_template_Images/saved_{i+20}.jpg', modifiedImg)
+	cv2.imwrite(f'generated_template_Images/saved_{i+40}.jpg', resizedImg)
+	#cv2.imshow("img", modified)
+
+	#sleep(1)
+cv2.imshow('winname', image)
+fimg = flipped(image)
+cv2.imshow('f', fimg)
+# cv2.imshow('oo', image[::-1])
+
 cv2.waitKey()
